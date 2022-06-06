0c58a98c5b0fc1c52347e8b153022dbd1a4fb772
ombujeong <ombujeong@gmail.com>
2022-06-06 22:30:04 +0900

    first commit

---
+++ src/test/java/com/dreamteam/hola/HolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamteam.hola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
