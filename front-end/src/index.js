20551e1a00c8200519b57ef08d22ef718c28da87
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-06-08 16:58:18 +0900

    2. 새글쓰기 Edit 완료

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-document.cookie = "safeCookie1foo; SameSite=Lax";
-document.cookie = "safeCookie1foo";
-document.cookie = "crossCookie=bar; SameSite=None; Secure";
+
 root.render(
   <React.StrictMode>
     <App />
