e90e3eb7e3094e237ac972d0fd239794500af06d
DESKTOP-K9D48KS\mycom <dnwlsrla40@naver.com>
2022-06-09 23:43:22 +0900

    [dev] hola 평가 mail sender 구현

---
+++ back-end/src/main/java/com/dreamteam/hola/controller/MailController.java
@@ -0,0 +1,24 @@
+package com.dreamteam.hola.controller;
+
+import com.dreamteam.hola.dto.MailDto;
+import com.dreamteam.hola.service.MailServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.mail.MessagingException;
+
+@RequiredArgsConstructor
+@Controller
+public class MailController {
+    private final MailServiceImpl mailServiceImpl;
+
+    @PostMapping("/mail")
+    public ResponseEntity<?> mail(@RequestBody MailDto mailDto) throws MessagingException {
+        mailServiceImpl.sendMail(mailDto);
+        return new ResponseEntity<>("success", HttpStatus.OK);
+    }
+}
