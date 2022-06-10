c15a8a48158a1b76b83db7b7bdafe025ccf8af5a
younjaeyeal <jyyoun1021@gmail.com>
2022-06-11 07:51:49 +0900

    Member 전면수정

--- back-end/src/main/java/com/dreamteam/hola/domain/Member.java
+++ back-end/src/main/java/com/dreamteam/hola/domain/Member.java
@@ -1,16 +1,26 @@
 package com.dreamteam.hola.domain;
 
+import lombok.*;
+
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Member {
-    private String memberId;
+    private Long memberNo;
+    private String username;
     private String nickname;
     private String password;
     private String email;
-    private String profile_image;
-    private LocalDate reg_date;
-    private LocalDate mod_date;
-    private char withdrawal_yn;
-    private LocalDate withdrawal_date;
-    private String social_type;
+    private String profileImage;
+    private Role role;
+    private LocalDate regDate;
+    private LocalDate modDate;
+    private char withdrawalYn;
+    private LocalDate withdrawalDate;
+    private String socialType;//카카오,깃허브,구글
 }
