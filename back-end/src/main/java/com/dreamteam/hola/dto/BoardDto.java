7f9eb5dd7460a125b1aa5515ff00920bb6bbce27
ombujeong <ombujeong@gmail.com>
2022-06-22 02:16:57 +0900

    6.22 boardDto 변수 추가

--- back-end/src/main/java/com/dreamteam/hola/dto/BoardDto.java
+++ back-end/src/main/java/com/dreamteam/hola/dto/BoardDto.java
@@ -14,13 +14,17 @@ public class BoardDto {
     private Long  id;
 //    private String nickname;         //작성자 번호
     private String title;           //제목
+    private String content;
     private Long viewCnt;          // 조회수
     private String recruitStatus;    //모집 상태
     private String recruitType;    //모집 구분
     private String recruitCnt;     //모집 인원
+    private String progressType;
     private String duration;        //진행 기간
     private Date startDate;   //시작 예정일
     private int commentCnt;
+    private String contactType;
+    private String contact;
     private List<String> skills;
 
 
