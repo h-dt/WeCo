7549ddc0baf838ce4bb0ea52430c7108f16d2482
김우진 <dnwlsrla40@naver.com>
2022-06-24 20:50:46 +0900

    [update] skill 가져오는 로직 Skill class에서 String으로 변경

--- back-end/src/main/java/com/dreamteam/hola/service/BoardServiceImpl.java
+++ back-end/src/main/java/com/dreamteam/hola/service/BoardServiceImpl.java
@@ -4,7 +4,6 @@ import com.dreamteam.hola.dao.BoardMapper;
 import com.dreamteam.hola.dao.CommentMapper;
 import com.dreamteam.hola.dao.SkillMapper;
 import com.dreamteam.hola.domain.Comment;
-import com.dreamteam.hola.domain.Skill;
 import com.dreamteam.hola.dto.BoardDetailDto;
 import com.dreamteam.hola.dto.BoardDto;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
@@ -28,13 +28,9 @@ public class BoardServiceImpl implements BoardService {
         BoardDetailDto findBoard = boardMapper.findById(id);
         boardMapper.updateBoardViewCnt(id);
         List<Comment> comments = commentMapper.findAllByBoardId(id);
-        List<Skill> skills = skillMapper.findAllByBoardId(id);
-        List<String> transSkills = new ArrayList<>();
+        List<String> skills = skillMapper.findAllByBoardId(id);
         findBoard.setComments(comments);
-        for (Skill skill : skills) {
-            transSkills.add(skill.getSkillType());
-        }
-        findBoard.setSkills(transSkills);
+        findBoard.setSkills(skills);
         return findBoard;
     }
 
@@ -43,13 +39,9 @@ public class BoardServiceImpl implements BoardService {
     public List<BoardDto> getBoardListByRecruitType(String recruitType) {
         List<BoardDto> allByRecruitType = boardMapper.findAllByRecruitType(recruitType);
         for (BoardDto boardDto : allByRecruitType) {
-            List<Skill> skills = skillMapper.findAllByBoardId(boardDto.getId());
-            List<String> transSkills = new ArrayList<>();
-            for (Skill skill : skills) {
-                transSkills.add(skill.getSkillType());
-            }
+            List<String> skills = skillMapper.findAllByBoardId(boardDto.getId());
             boardDto.setCommentCnt(commentMapper.CountByBoardId(boardDto.getId()));
-            boardDto.setSkills(transSkills);
+            boardDto.setSkills(skills);
         }
         return allByRecruitType;
     }
