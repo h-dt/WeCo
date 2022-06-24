ff6c57b35ee1963df9fb11dd3c9e8e9b071cdc7d
김우진 <dnwlsrla40@naver.com>
2022-06-24 20:52:29 +0900

    [update] comment로직에 validation 추가

--- back-end/src/main/java/com/dreamteam/hola/dto/CommentDto.java
+++ back-end/src/main/java/com/dreamteam/hola/dto/CommentDto.java
@@ -5,12 +5,23 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class CommentDto {
 //    private Long memeber_id;
+
+    private Long commentId;
+
+    @NotNull
+    @Positive
     private Long boardId;
+
+    @NotBlank
     private String content;
 }
