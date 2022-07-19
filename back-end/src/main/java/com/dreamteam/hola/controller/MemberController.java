d174da32d2530999f0ebe67cb09623da217ad8cc
younjaeyeal <jyyoun1021@gmail.com>
2022-07-19 16:04:16 +0900

    프로필과 프로필 수정 완료

--- back-end/src/main/java/com/dreamteam/hola/controller/MemberController.java
+++ back-end/src/main/java/com/dreamteam/hola/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.dreamteam.hola.controller;
 
+import com.dreamteam.hola.config.auth.PrincipalDetails;
 import com.dreamteam.hola.dto.MemberDto;
 import com.dreamteam.hola.service.MemberService;
 import com.dreamteam.hola.util.jwt.Token;
@@ -7,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -34,5 +36,20 @@ public class MemberController {
             return new ResponseEntity<>("success", HttpStatus.CREATED);
         else
             return new ResponseEntity<>("fail", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getProfile(@PathVariable("id")Long id){
+        log.info(" 회원가져오기 id={}",id);
+        MemberDto result = memberServiceImpl.getProfile(id);
+        return new ResponseEntity<>(result,HttpStatus.OK);
+    }
+
+    @PutMapping("/member/{id}")
+    public ResponseEntity<?> updateMember(@PathVariable("id")Long id,
+                                          @RequestPart("key")MemberDto memberDto,
+                                          @RequestPart(value = "file",required = false)MultipartFile multipartFile
+                                         ) throws IOException {
+        memberServiceImpl.update(id,memberDto,multipartFile);
+        return  new ResponseEntity<>("success",HttpStatus.OK);
     }
 }
