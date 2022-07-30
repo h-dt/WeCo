78a7ce91d69d58f1d312c4a507c7f58e4f1a7413
Ji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-07-30 18:28:38 +0900

    feat : skill 선택하는 부분 생성
    스타일 지정 + 스킬 선택 마무리
    
    이제는 스킬을 클릭하여 정렬하는 기능 구현해야함

--- weco-app/pages/index.tsx
+++ weco-app/pages/index.tsx
@@ -6,11 +6,16 @@ import RecruitMain from './recruit';
 import RecruitCard from './recruit/RecruitCard';
 import RecruitHeader from './recruit/RecruitHeader';
 
+import type { NextPage } from 'next';
+import { SkillSelect } from '../components/SkillSelect';
+
 const Home: NextPage = () => {
   return (
     <>
       {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
       <RecruitMain />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <SkillSelect />
     </>
   );
 };
