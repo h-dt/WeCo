857e66b76b862f248e8213a3e52836e2b175628a
Ji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-07-31 20:42:23 +0900

    chore : react slick, slick carosel 설치
    타입스크립트용 slider 설치

--- weco-app/pages/index.tsx
+++ weco-app/pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
+import { MainHeader } from "../components/MainHeader";
 import { SkillSelect } from "../components/SkillSelect";
 
 const Home: NextPage = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <MainHeader />
       <SkillSelect />
     </>
   );
