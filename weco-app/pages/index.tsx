d55103277c4241a461c4d3b6dc077a5165169e52
Ji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-07-31 20:57:56 +0900

    Feat : Slick 슬라이더 이용해서 slider 만들기

--- weco-app/pages/index.tsx
+++ weco-app/pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from "next";
 import { MainHeader } from "../components/MainHeader";
+import { MainSlider } from "../components/MainSlider";
 import { SkillSelect } from "../components/SkillSelect";
 
 const Home: NextPage = () => {
   return (
     <>
       <MainHeader />
+      <MainSlider />
       <SkillSelect />
     </>
   );
