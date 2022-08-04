af08c8dbe04911cdda68c9a4bc5691d3ba52782a
younjaeyeal <jyyoun1021@gmail.com>
2022-08-04 18:28:45 +0900

    유효성 검사 및 예외처리 (LinkedHashMap 을 써서 순서대로 나오게

--- back-end/src/main/java/com/dreamteam/hola/controller/BoardController.java
+++ back-end/src/main/java/com/dreamteam/hola/controller/BoardController.java
@@ -96,11 +96,7 @@ public class BoardController {
 
     }
 
-    @ExceptionHandler(NullPointerException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    protected ErrorResponse handlerException1(){
-        return ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR,"존재하지 않는 글입니다.");
-    }
+
 
 
 }
