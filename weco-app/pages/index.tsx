4f79b19c14d36fef1c551e6ddef62fae9ed3d6af
Ji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-08-04 22:11:20 +0900

    refactor : Slider Modules 수정, Select 수정

--- weco-app/pages/index.tsx
+++ weco-app/pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from "next";
-import { MainHeader } from "../components/MainHeader";
-import { MainSlider } from "../components/MainSlider";
-import { SkillSelect } from "../components/SkillSelect";
+import type { NextPage } from 'next';
+import { MainHeader } from '../components/Header/MainHeader';
+import { MainSlider } from '../components/Slider/MainSlider';
+import { SkillSelect } from '../components/Skill/SkillSelect';
 
 const Home: NextPage = () => {
   return (
