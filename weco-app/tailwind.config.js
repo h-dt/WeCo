46af7d64a814e7412564ee89d4144ff3e173afde
dongmin <rlaehdals753@naver.com>
2022-08-05 01:48:27 +0900

    fix: tailwind.comfig 파일 복구

--- weco-app/tailwind.config.js
+++ weco-app/tailwind.config.js
@@ -5,16 +5,28 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens: {
+      xm: { max: '554px' },
+      sm: { max: '767px' },
+      // => @media (min-width: 640px and max-width: 767px) { ... }
 
-     
+      md: { min: '768px', max: '1023px' },
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      lg: { min: '1024px', max: '1279px' },
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      xl: { min: '1280px', max: '1535px' },
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+      '2xl': { min: '1536px' },
+      // => @media (min-width: 1536px) { ... }
     },
-
     extend: {
       colors: {
         kakao: '#FEE500',
       },
     },
   },
-  plugins: [],
-
+  plugins: [require('@tailwindcss/line-clamp')],
 };
