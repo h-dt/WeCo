579e44e2089a97fde986db6f98a770dc76205cf5
Ji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-08-05 16:06:40 +0900

    fix : git 오류 수정

--- weco-app/pages/index.tsx
+++ weco-app/pages/index.tsx
@@ -4,20 +4,10 @@ import styles from '../styles/Home.module.css';
 import RecruitMain from './recruit';
 import RecruitCard from './recruit/RecruitCard';
 import RecruitHeader from './recruit/RecruitHeader';
-
 import type { NextPage } from 'next';
-
-import { MainSlider } from '../components/MainSlider';
-
+import { MainSlider } from '../components/Slider';
 import { MainHeader } from '../components/Header';
-
 import { SkillSelect } from '../components/Skill';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-import RecruitMain from './recruit';
-import RecruitCard from './recruit/RecruitCard';
-import RecruitHeader from './recruit/RecruitHeader';
 
 const Home: NextPage = () => {
   return (
