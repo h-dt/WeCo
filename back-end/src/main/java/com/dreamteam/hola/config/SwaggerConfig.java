3ffebe3c89b42b549239e8ecb2d0f0e92e50b827
DESKTOP-K9D48KS\mycom <dnwlsrla40@naver.com>
2022-08-11 18:58:25 +0900

    [dev] #81 board swagger 1차 구현 완료(김우진 로직 완)

--- back-end/src/main/java/com/dreamteam/hola/config/SwaggerConfig.java
+++ back-end/src/main/java/com/dreamteam/hola/config/SwaggerConfig.java
@@ -3,12 +3,18 @@ package com.dreamteam.hola.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -20,6 +26,31 @@ public class SwaggerConfig {
                 .description("API 상세소개 및 사용법")
                 .version("1.0")
                 .build();
+    }
+
+    @Bean
+    public Docket needTokenApi() {
+        ParameterBuilder parameterBuilder = new ParameterBuilder();
+        parameterBuilder
+                .name("X-AUTH-TOKEN")
+                .description("JWT Access Token")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+//                .required(true)
+                .build();
+
+        List<Parameter> parameters = new ArrayList<>();
+        parameters.add(parameterBuilder.build());
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("Token Api")
+                .globalOperationParameters(parameters)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.dreamteam.hola"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(commonInfo());
     }
 
     @Bean
--- back-end/src/main/java/com/dreamteam/hola/exception/BoardControlAdvice.java
+++ back-end/src/main/java/com/dreamteam/hola/exception/BoardControlAdvice.java
@@ -2,6 +2,7 @@ package com.dreamteam.hola.exception;
 
 import com.dreamteam.hola.controller.BoardController;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,4 +16,9 @@ public class BoardControlAdvice {
         return ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR,"존재하지 않는 글입니다.");
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ErrorResponse handlerException2() {
+        return ErrorResponse.of(HttpStatus.BAD_REQUEST, "올바르지 않은 매개변수 형식입니다.");
+    }
 }
