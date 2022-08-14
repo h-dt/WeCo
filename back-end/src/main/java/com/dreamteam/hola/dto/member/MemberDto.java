aa4b3c4cc7199f2d1f5c0abde03e0829a43b02a9
younjaeyeal <jyyoun1021@gmail.com>
2022-08-14 18:54:30 +0900

    MemberDto 에서 username 삭 및 로그인 요청 dto 는 불가능하여 삭제

--- back-end/src/main/java/com/dreamteam/hola/dto/member/MemberDto.java
+++ back-end/src/main/java/com/dreamteam/hola/dto/member/MemberDto.java
@@ -28,7 +28,7 @@ public class MemberDto {
     private String password;
 
     @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
-    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @NotBlank
     private String email;
     private String profileImage;
     private Role role;
