bb5657500e0ce37ab2ef0c9cbc72b66fcab8bcd9
DESKTOP-K9D48KS\mycom <dnwlsrla40@naver.com>
2022-08-18 21:49:39 +0900

    [update] update 시 mode_date 시간 변경

--- back-end/src/main/java/com/dreamteam/hola/service/BoardServiceImpl.java
+++ back-end/src/main/java/com/dreamteam/hola/service/BoardServiceImpl.java
@@ -99,7 +99,7 @@ public class BoardServiceImpl implements BoardService {
         }
 
         Date date = Calendar.getInstance().getTime();
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String updateDate = dateFormat.format(date);
 
         boardDto.setModDate(updateDate);
