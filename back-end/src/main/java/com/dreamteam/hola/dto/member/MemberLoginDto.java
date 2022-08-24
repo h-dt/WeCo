b4c80b8eaf35e777391d72ad0a46fcb8f63754fb
DESKTOP-K9D48KS\mycom <dnwlsrla40@naver.com>
2022-08-24 16:35:05 +0900

    [update] 비밀번호 예시 변경

--- back-end/src/main/java/com/dreamteam/hola/dto/member/MemberLoginDto.java
+++ back-end/src/main/java/com/dreamteam/hola/dto/member/MemberLoginDto.java
@@ -22,6 +22,6 @@ public class MemberLoginDto {
     private String email;
 
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
-    @ApiModelProperty(value = "로그인 password", example = "1234")
+    @ApiModelProperty(value = "로그인 password", example = "asdfqwer1234~")
     private String password;
 }
